0a29feea172c79a0779996bd6cc788245fb64754
KALE-VINAY <vinaykale321@gmail.com>
2024-11-05 20:34:43 +0530

    add login UI good

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,103 +1,117 @@
-import React from 'react'
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import the carousel styles
 import RestaurantCard from './RestaurantCard';
 import Header from './Header';
 import Footer from './Footer';
 
 const Browse = () => {
+  const restaurants = [
+    {
+      name: 'Paakashala',
+      cuisines: 'South Indian, North Indian',
+      price: '₹100 for one',
+      rating: 4.4,
+      time: '31 min',
+      discount: '50% OFF',
+      imageUrl: 'https://media.istockphoto.com/id/867909720/photo/food-for-indian-festival-diwali.jpg?s=612x612&w=0&k=20&c=NQdkQQo5dNSRgLJsaqPHJ1JiU_v2ob7D529i4A4mPjY=',
+      promoted: true,
+    },
+    {
+      name: 'S N Refreshments',
+      cuisines: 'South Indian',
+      price: '₹40 for one',
+      rating: 4.6,
+      time: '24 min',
+      imageUrl: 'https://media.istockphoto.com/id/1271604943/photo/assorted-indian-home-food-different-dishes-and-snacks-wooden-rustic-table-homemade-pilaf.jpg?s=612x612&w=0&k=20&c=f2aFRB_7ooZ01F3NA1grsLBwbXhnJuXj5VoalTKSYxo=',
+      promoted: false,
+    },
+    {
+      name: 'Namaste',
+      cuisines: 'South Indian, North Indian',
+      price: '₹150 for one',
+      rating: 4.3,
+      time: '36 min',
+      discount: '40% OFF',
+      imageUrl: 'https://media.istockphoto.com/id/1005630488/photo/thali.jpg?s=612x612&w=0&k=20&c=ufwOsc-f-c7ibq5vzsQ1y8VTC3o61RtgN3YhVa_U5ms=',
+      promoted: true,
+    },
+    {
+      name: 'Namaste',
+      cuisines: 'South Indian, North Indian',
+      price: '₹150 for one',
+      rating: 4.3,
+      time: '36 min',
+      discount: '40% OFF',
+      imageUrl: 'https://media.istockphoto.com/id/1408964184/photo/chicken-tikka-masala-kadai-chicken-coconut-sabdji-indias-samosa-veg-curry-bainkar-bharta.jpg?s=612x612&w=0&k=20&c=hzCybSQollGMvUfvVTONbD7R96G63gCXgC5XPwdOkBo=',
+      promoted: true,
+    },
+    {
+      name: 'Namaste',
+      cuisines: 'South Indian, North Indian',
+      price: '₹150 for one',
+      rating: 4.3,
+      time: '36 min',
+      discount: '40% OFF',
+      imageUrl: 'https://media.istockphoto.com/id/1490687071/photo/top-view-for-oriental-food.jpg?s=612x612&w=0&k=20&c=9yVib9aJvvzCPtKSpNTKavb6BUPFqe2pOr4LKRT4gPY=',
+      promoted: true,
+    },
+    {
+      name: 'Namaste',
+      cuisines: 'South Indian, North Indian',
+      price: '₹150 for one',
+      rating: 4.3,
+      time: '36 min',
+      discount: '40% OFF',
+      imageUrl: 'https://media.istockphoto.com/id/922783734/photo/assorted-indian-recipes-food-various.jpg?s=2048x2048&w=is&k=20&c=1B9sUUPUsoVBAMCsk461nPRix-YIo74i8LgSWSkhCOE=',
+      promoted: true,
+    },
+    {
+      name: 'Namaste',
+      cuisines: 'South Indian, North Indian',
+      price: '₹150 for one',
+      rating: 4.3,
+      time: '36 min',
+      discount: '40% OFF',
+      imageUrl: 'https://media.istockphoto.com/id/1442604378/photo/food-photos-various-entrees-appetizers-deserts-etc.jpg?s=612x612&w=0&k=20&c=WUCuJ2L2VKlrsIsXVnYX1HoG5rZM8K6ISeiE1liMBq4=',
+      promoted: true,
+    },
+  ];
 
-    const restaurants = [
-        {
-          name: 'Paakashala',
-          cuisines: 'South Indian, North Indian',
-          price: '₹100 for one',
-          rating: 4.4,
-          time: '31 min',
-          discount: '50% OFF',
-          imageUrl: 'https://media.istockphoto.com/id/867909720/photo/food-for-indian-festival-diwali.jpg?s=612x612&w=0&k=20&c=NQdkQQo5dNSRgLJsaqPHJ1JiU_v2ob7D529i4A4mPjY=', // Replace with actual image URL
-          promoted: true,
-        },
-        {
-          name: 'S N Refreshments',
-          cuisines: 'South Indian',
-          price: '₹40 for one',
-          rating: 4.6,
-          time: '24 min',
-          imageUrl: 'https://media.istockphoto.com/id/1271604943/photo/assorted-indian-home-food-different-dishes-and-snacks-wooden-rustic-table-homemade-pilaf.jpg?s=612x612&w=0&k=20&c=f2aFRB_7ooZ01F3NA1grsLBwbXhnJuXj5VoalTKSYxo=', // Replace with actual image URL
-          promoted: false,
-        },
-        {
-          name: 'Namaste',
-          cuisines: 'South Indian, North Indian',
-          price: '₹150 for one',
-          rating: 4.3,
-          time: '36 min',
-          discount: '40% OFF',
-          imageUrl: 'https://media.istockphoto.com/id/1005630488/photo/thali.jpg?s=612x612&w=0&k=20&c=ufwOsc-f-c7ibq5vzsQ1y8VTC3o61RtgN3YhVa_U5ms=', // Replace with actual image URL
-          promoted: true,
-        },
-        {
-          name: 'Namaste',
-          cuisines: 'South Indian, North Indian',
-          price: '₹150 for one',
-          rating: 4.3,
-          time: '36 min',
-          discount: '40% OFF',
-          imageUrl: 'https://media.istockphoto.com/id/1408964184/photo/chicken-tikka-masala-kadai-chicken-coconut-sabdji-indias-samosa-veg-curry-bainkar-bharta.jpg?s=612x612&w=0&k=20&c=hzCybSQollGMvUfvVTONbD7R96G63gCXgC5XPwdOkBo=', // Replace with actual image URL
-          promoted: true,
-        },
-        {
-          name: 'Namaste',
-          cuisines: 'South Indian, North Indian',
-          price: '₹150 for one',
-          rating: 4.3,
-          time: '36 min',
-          discount: '40% OFF',
-          imageUrl: 'https://media.istockphoto.com/id/1490687071/photo/top-view-for-oriental-food.jpg?s=612x612&w=0&k=20&c=9yVib9aJvvzCPtKSpNTKavb6BUPFqe2pOr4LKRT4gPY=', // Replace with actual image URL
-          promoted: true,
-        },
-        {
-          name: 'Namaste',
-          cuisines: 'South Indian, North Indian',
-          price: '₹150 for one',
-          rating: 4.3,
-          time: '36 min',
-          discount: '40% OFF',
-          imageUrl: 'https://media.istockphoto.com/id/922783734/photo/assorted-indian-recipes-food-various.jpg?s=2048x2048&w=is&k=20&c=1B9sUUPUsoVBAMCsk461nPRix-YIo74i8LgSWSkhCOE=', // Replace with actual image URL
-          promoted: true,
-        },
-        {
-          name: 'Namaste',
-          cuisines: 'South Indian, North Indian',
-          price: '₹150 for one',
-          rating: 4.3,
-          time: '36 min',
-          discount: '40% OFF',
-          imageUrl: 'https://media.istockphoto.com/id/1442604378/photo/food-photos-various-entrees-appetizers-deserts-etc.jpg?s=612x612&w=0&k=20&c=WUCuJ2L2VKlrsIsXVnYX1HoG5rZM8K6ISeiE1liMBq4=', // Replace with actual image URL
-          promoted: true,
-        },
-        // Add more restaurants as needed
-      ];
+  const carouselImages = [
+    'https://media.istockphoto.com/id/854033828/photo/collage-of-food-products.webp?a=1&b=1&s=612x612&w=0&k=20&c=UlsaFrbeoA1GWsBB2zdAxa2ftskWPCb_0Ixrkc4-6ys=',
+    'https://tse4.mm.bing.net/th?id=OIP.KiXKWwQ4bjHKjYGdGAhOQwHaCU&pid=Api&P=0&h=180',
+    'https://media.istockphoto.com/id/922783734/photo/assorted-indian-recipes-food-various.jpg?s=2048x2048&w=is&k=20&c=1B9sUUPUsoVBAMCsk461nPRix-YIo74i8LgSWSkhCOE=',
+  ];
 
   return (
-    <div >
-     <Header/>
-    <div className="min-h-screen bg-white p-2 mb-10">
-     
-        <img className='min-w-full rounded-xl mt-28' src="https://media.istockphoto.com/id/854033828/photo/collage-of-food-products.webp?a=1&b=1&s=612x612&w=0&k=20&c=UlsaFrbeoA1GWsBB2zdAxa2ftskWPCb_0Ixrkc4-6ys="/>
-      
-      
-      <h1 className="text-5xl  font-serif my-14 text-center ">Restaurants near TU</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-5">
-        {restaurants.map((restaurant, index) => (
-          <RestaurantCard key={index} {...restaurant} />
-        ))}
+    <div>
+      <Header />
+      <div className="min-h-screen bg-white p-2 mb-10">
+        <Carousel
+          showThumbs={false}
+          showStatus={false}
+          autoPlay
+          infiniteLoop
+          className="rounded-xl mt-28"
+        >
+          {carouselImages.map((src, index) => (
+            <div key={index}>
+              <img src={src} alt={`Carousel ${index + 1}`} className="min-w-full h-96 rounded-xl" />
+            </div>
+          ))}
+        </Carousel>
+        
+        <h1 className="text-5xl font-serif my-14 text-center">Restaurants near TU</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-5">
+          {restaurants.map((restaurant, index) => (
+            <RestaurantCard key={index} {...restaurant} />
+          ))}
+        </div>
       </div>
+      <Footer />
     </div>
-    <Footer />
-    
-    </div>
-   
   );
-}
+};
 
-export default Browse
+export default Browse;
--- src/components/Login.js
+++ src/components/Login.js
@@ -153,47 +153,48 @@ const Login = () => {
 
   return (
     <div>
-       <Header />
+      
       <div className="absolute">
         <img className=' h-screen  md:w-screen object-cover '
           src='https://media.istockphoto.com/id/1169694902/photo/assorted-indian-non-vegetarian-food-recipe-served-in-a-group-includes-chicken-curry-mutton.webp?a=1&b=1&s=612x612&w=0&k=20&c=u_Txs8ayelMVBzmb142cbiE7UO4vDokq9hXUCUREPEc=' alt="bg img"
         />
       </div>
 
-      <form onSubmit={(e) => e.preventDefault()} className=" w-8/12 md:w-3/12 absolute p-6 md:p-12 bg-white my-36 md:my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
-        <h1 className="font-bold text-3xl py-4 ">
+      <form onSubmit={(e) => e.preventDefault()} className=" w-8/12 md:w-3/12 absolute p-6 md:p-12  bg-gradient-to-l from bg-yellow-50 my-36 md:my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80 border-black">
+        <h1 className="font-bold font-serif text-4xl py-4 text-gray-800 ">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
 
-       { !isSignInForm && (<input 
-          ref={name}
-          type="text" 
-          placeholder="Full name" 
-          className=" p-4 my-4 w-full bg-gray-700 rounded-lg"
-        />)}
+        { !isSignInForm && (
+            <input 
+              ref={name}
+              type="text" 
+              placeholder="Full name" 
+              className="p-4 my-4 w-full bg-white text-gray-800 rounded-lg" // Added text color
+            />
+          )}
 
+          <input 
+            ref={email}
+            type="text" 
+            placeholder="Email address" 
+            className="p-4 my-4 w-full bg-white text-gray-800 rounded-lg" // Added text color
+          />
 
-        <input 
-        ref={email}
-          type="text" 
-          placeholder="Email address" 
-          className=" p-4 my-4 w-full bg-gray-700 rounded-lg"
-        />
+          <input 
+            ref={password}
+            type="password" 
+            placeholder="Password" 
+            className="p-4 my-4 w-full bg-white text-gray-800 rounded-lg" // Added text color
+          />
 
-        <input 
-        ref={password}
-          type="password" 
-          placeholder="Password" 
-          className=" p-4 my-4 w-full bg-gray-700 rounded-lg"
-        />
-        
         <p className="text-red-500 font-bold text-lg py-2" >{errorMessage}</p>
 
-        <button className=" p-4 my-6 bg-red-700 w-full rounded-lg" onClick={handleButtonClick}>
+        <button className=" p-4 my-6 bg-yellow-300 w-full rounded-lg" onClick={handleButtonClick}>
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
-        <p className=" py-4 cursor-pointer" onClick={toggleSignInForm}>
-          {isSignInForm ? "New to Netflix? Sign up now" : "Already a member? Sign in"}
+        <p className=" py-4 cursor-pointer text-black" onClick={toggleSignInForm}>
+          {isSignInForm ? "New to here? Sign up now" : "Already a member? Sign in"}
         </p>
       </form>
     </div>
