0c70fe5d3a4d748ec65a7205d5a4ffc1277ae271
KALE-VINAY <vinaykale321@gmail.com>
2024-12-26 16:21:24 +0530

    add changes

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -11,12 +11,19 @@ import { getStorage } from 'firebase/storage';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY_FINDER_APP_TU,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN_FINDER_APP_TU,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID_FINDER_APP_TU,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET_FINDER_APP_TU,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID_FINDER_APP_TU,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID_FINDER_APP_TU,
+  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY_FINDER_APP_TU,
+  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN_FINDER_APP_TU,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID_FINDER_APP_TU,
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET_FINDER_APP_TU,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID_FINDER_APP_TU,
+  // appId: process.env.REACT_APP_FIREBASE_APP_ID_FINDER_APP_TU,
+  // measurementId: "G-9VLMM5WB1R"
+  apiKey: "AIzaSyCkYgLye1IiQazbJtxDleByvqO7R0A2Fr0",
+  authDomain: "food-app-tu.firebaseapp.com",
+  projectId: "food-app-tu",
+  storageBucket: "food-app-tu.firebasestorage.app",
+  messagingSenderId: "172520369779",
+  appId: "1:172520369779:web:46de1c99cb7b2fdcee8406",
   measurementId: "G-9VLMM5WB1R"
 };
 
